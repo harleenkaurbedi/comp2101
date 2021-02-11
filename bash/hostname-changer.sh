71624acee464faac8d6fbfee494b986181a243bf
Harleen bedi <harleenbedi980@gmail.com>
2021-02-11 12:13:48 -0500

    lab 2 file

--- bash/hostname-changer.sh
+++ bash/hostname-changer.sh
@@ -6,17 +6,30 @@
 # Send your script to your github repo, and submit the URL with screenshot on Blackboard
 
 # Get the current hostname using the hostname command and save it in a variable
+hostname=$(cat /etc/hostname)
 
 # Tell the user what the current hostname is in a human friendly way
+echo "Current Hostname is $hostname"
 
 # Ask for the user's student number using the read command
+echo "What is your student no."
+read number
 
 # Use that to save the desired hostname of pcNNNNNNNNNN in a variable, where NNNNNNNNN is the student number entered by the user
+hostnamectl set-hostname pc$number
+newhostname=$(hostname)
+echo "new hostname is: $newhostname"
 
 # If that hostname is not already in the /etc/hosts file, change the old hostname in that file to the new name using sed or something similar and
 #     tell the user you did that
 #e.g. sed -i "s/$oldname/$newname/" /etc/hosts
+echo "changing the hostname"
+sudo sed -i "s/$hostname/$newhostname/g" /etc/hosts
+echo "hostname changed"
+echo "successfull"
 
 # If that hostname is not the current hostname, change it using the hostnamectl command and
 #     tell the user you changed the current hostname and they should reboot to make sure the new name takes full effect
 #e.g. hostnamectl set-hostname $newname
+read -s -n 1 -p "Press any key to reboot"
+sudo reboot
